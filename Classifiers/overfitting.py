6daf7ee58c2750f0e6b55386e0dc3913f5e1fc83
patrick <patrick@moneypenny.(none)>
2013-12-17 15:16:06 -0500

    overfitting complete with example images

--- Classifiers/overfitting.py
+++ Classifiers/overfitting.py
@@ -1,17 +1,28 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-	x = np.linspace(-4,4)
-	x_data = x[::5]
-	y_data = x_data**2 + np.random.randn(*x_data.shape)
+# generate data along a quadratic
+x = np.linspace(-1.5,1.5,99)
+x_data = x[::4]
+y_data = 2*x_data**2 + .25*np.random.randn(*x_data.shape)
 
-	coeff = np.polyfit(x_data,y_data,2)
-	y_fit = sum([x*c**i for i,c in enumerate(coeff[::-1])])
-	coeff = np.polyfit(x_data,y_data,len(x_data)-1)
-	y_ovrfit = sum([x*c**i for i,c in enumerate(coeff[::-1])])
-	print y_ovrfit
+# fit the data
+coeff = np.polyfit(x_data,y_data,4)
+y_fit = sum([c*x**i for i,c in enumerate(coeff[::-1])])
+coeff = np.polyfit(x_data,y_data,len(x_data)-1)
+y_ovrfit = sum([c*x**i for i,c in enumerate(coeff[::-1])])
+print y_fit
 
-	# plt.plot(x,y)
-	# plt.ylabel('some numbers')
-	# plt.show()
+# plot and label figures
+plt.plot(x_data,y_data,'.')
+plt.title('Well-Tuned Model')
+plt.plot(x,y_fit)
+plt.axis([-1.1,1.1,-.5,3])
+
+plt.figure()
+plt.plot(x_data,y_data,'.')
+plt.title('Overfit Model')
+plt.plot(x,y_ovrfit)
+plt.axis([-1.1,1.1,-.5,3])
+
+plt.show()
